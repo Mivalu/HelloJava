9fbadf51ea1553f453fc69c6a5c444af6198689c
Michele Lugo <peachyjello@gmail.com>
2016-09-13 20:21:23 -0400

    Made a small change.

--- helloWorld.java
+++ helloWorld.java
@@ -3,5 +3,6 @@ public class helloWorld {
 	public static void main(String [] args){
 		System.out.print("Hello World.");
 		//It prints a line.
+		System.out.print("Using terminal in Eclipse. Weird.");
 	}
 }
